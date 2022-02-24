44cd2604b897d6f1cae056ff0d794eced514931d
floriangtmegmail <floriangtme@gmail.com>
2022-02-24 14:47:19 +0100

    1er version

---
+++ src/main/java/com/cda/contenu_seance/ContenuSeanceApplication.java
@@ -0,0 +1,13 @@
+package com.cda.contenu_seance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContenuSeanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContenuSeanceApplication.class, args);
+    }
+
+}
