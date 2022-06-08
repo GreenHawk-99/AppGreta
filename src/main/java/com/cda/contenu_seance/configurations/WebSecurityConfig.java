c5ce1ad87467d8bbc7b32050801bf608dfd65dce
GreenHawk <greenhawk99.mb@gmail.com>
2022-06-08 15:55:51 +0200

    auto

--- src/main/java/com/cda/contenu_seance/configurations/WebSecurityConfig.java
+++ src/main/java/com/cda/contenu_seance/configurations/WebSecurityConfig.java
@@ -1,14 +1,9 @@
 package com.cda.contenu_seance.configurations;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/home", "/h2-console", "/h2-console/**", "/webjars/bootstrap/5.1.3/**").permitAll()
+                .antMatchers("/home", "/h2-console", "/h2-console/**", "/webjars/bootstrap/5.1.3/**", "/static/**").permitAll()
                 .antMatchers("/formateur/**", "/welcome").hasAnyAuthority("Formateur","Coordinateur")
                 .antMatchers("/coordonateur/**", "/formateur/**").hasAnyAuthority("Coordinateur")
                 .antMatchers("/admin/**").hasAnyAuthority("Coordinateur")
@@ -46,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutUrl("/logout")
+                .logoutSuccessUrl("/home")
                 .permitAll()
                 .and()
                 .csrf().ignoringAntMatchers("/h2-console/**");
